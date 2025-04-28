425f9ba9d74f4a48659ebbe15f9608400c453c03
Syed Asif <sydasif78@gmail.com>
2025-04-28 08:13:13 +0500

    update if else logic

--- network_tools/views.py
+++ network_tools/views.py
@@ -34,20 +34,24 @@ def ping(request):
 def ip_addr(request):
     test_result = {}
 
-    if request.GET.get("cidr") == "":
-        test_result["test_result"] = "Please a valid CIDR value [1-24]"
-    elif "cidr" in request.GET:
+    if request.GET.get("cidr"):
         cidr = int(request.GET["cidr"])
         netmask_result = cidr_to_netmask(cidr)
         test_result["netmask_result"] = (
             f"Netmask for CIDR value {cidr} is {netmask_result}"
         )
-    elif "netmask" in request.GET:
+
+    if request.GET.get("netmask"):
         netmask = request.GET["netmask"]
         if is_netmask(netmask) is False:
             test_result["cidr_result"] = "Please enter a valid Netmask"
-            return render(request, "network_tools/ip_addr.html", test_result)
-        cidr_value = netmask_to_cidr(netmask)
-        test_result["cidr_result"] = f"CIDR Value for netmask {netmask} is {cidr_value}"
+        else:
+            cidr_value = netmask_to_cidr(netmask)
+            test_result["cidr_result"] = (
+                f"CIDR Value for netmask {netmask} is {cidr_value}"
+            )
+
+    if not test_result:
+        test_result["test_result"] = "Please provide a CIDR or Netmask value."
 
     return render(request, "network_tools/ip_addr.html", test_result)
